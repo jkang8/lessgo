e2103a1d8d077adf8f1465817d39167871f53191
jkang8 <johnkang.h@gmail.com>
2017-06-27 00:43:00 -0700

    Implement up/down scroll, put lines data and methods in struct

--- less.go
+++ less.go
@@ -3,37 +3,84 @@ package main
 import (
   "github.com/nsf/termbox-go"
   "bufio"
-  //"io"
-  //"io/ioutil"
   "os"
   "log"
-  //"reflect"
   //"fmt"
 )
 
+// Lines struct
+type Lines struct {
+  lines []string
+  lines_focus []string
+  min int
+  max int
+}
+
+func NewLines(min int, max int) *Lines {
+  l := new(Lines)
+  l.lines = make([]string, 0)
+  l.lines_focus = make([]string, 0)
+  l.min = min
+  l.max = max
+  return l
+}
+
+func (l *Lines) scrollDown() []string {
+  l.min++
+  l.max++
+  l.lines_focus = l.lines[l.min:l.max]
+  return l.lines_focus
+}
+
+func (l *Lines) scrollUp() []string {
+  l.min--
+  l.max--
+  l.lines_focus = l.lines[l.min:l.max]
+  return l.lines_focus
+}
+
+func (l *Lines) append(text string) {
+    l.lines = append(l.lines, text)
+}
+
+// Main
 func drawLine(text string, line_number int) {
-  //const coldef = termbox.ColorDefault
-  //termbox.Clear(coldef, coldef)
-  //w, h := termbox.Size()
   for pos, char := range text {
       termbox.SetCell(pos, line_number, char, 'b', 'a')
   }
   termbox.Flush()
 }
 
-func initial(pages []string, scanner *bufio.Scanner, term_height int, line_number int) (int) {
+func initial(linesObject *Lines, scanner *bufio.Scanner, term_height int, line_number int) (*Lines) {
         for line_number < term_height && scanner.Scan() {
             text := scanner.Text()
-						pages = append(pages, text)
-            //pages[line_number] = text
+            linesObject.append(text)
             drawLine(text, line_number)
 						line_number++
         }
-				return line_number
+				return linesObject
 }
 
-func scrollDown(pages []string, next_line string, line_number int) {
-		drawLine(next_line, line_number)
+func handleScrollDown(lines *Lines, next_line string) {
+    //termbox.Flush()
+    termbox.Clear('b', 'a')
+    counter := 0
+    //current_lines := lines[1:line_number+1]
+    current_lines := lines.scrollDown()
+    for _, str := range current_lines {
+      drawLine(string(str), counter)
+      counter++
+    }
+}
+
+func handleScrollUp(lines *Lines) {
+    termbox.Clear('b', 'a')
+    counter := 0
+    current_lines := lines.scrollUp()
+    for _, str := range current_lines {
+      drawLine(string(str), counter)
+      counter++
+    }
 }
 
 func main() {
@@ -44,37 +91,38 @@ func main() {
 
     args := os.Args[1:]
     filename := args[0]
-    //pages := make(map[int]string)
-		var pages []string
 		_, term_height := termbox.Size()
+		//var lines []string
+    //lines := Lines{lines: make([]string, 0), min: 0, max: term_height}
+    lines := NewLines(0, term_height)
 
     if file, err := os.Open(filename); err == nil {
         scanner := bufio.NewScanner(file)
-				line_number := initial(pages, scanner, term_height, 0)
+				lines := initial(lines, scanner, term_height, 0)
 
 				// main loop
 				listener:
 				for {
 						switch ev := termbox.PollEvent(); ev.Type {
-								case termbox.EventKey:
-										switch ev.Key {
-												case termbox.KeyEsc:
-														break listener
-												case termbox.KeyArrowDown:
-														scanner.Scan()
-														next_line := scanner.Text()
-														line_number++
-														scrollDown(pages, next_line, line_number)
-										}
-
-								case termbox.EventResize:
-								}
+            case termbox.EventKey:
+                switch ev.Key {
+                    case termbox.KeyEsc:
+                        break listener
+                    case termbox.KeyArrowDown:
+                        scanner.Scan()
+                        next_line := scanner.Text()
+                        lines.append(next_line)
+                        handleScrollDown(lines, next_line)
+                    case termbox.KeyArrowUp:
+                        handleScrollUp(lines)
+                }
+            case termbox.EventResize:
+            }
 				}
         defer file.Close()
     } else {
         log.Fatal(err)
     }
-
 
     defer termbox.Close()
 }
